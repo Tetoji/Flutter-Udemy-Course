c09d87269e57d1b8490b805d9245b55e271bab08
tjschulz <tjschulz@hansalog.de>
2024-01-30 08:39:29 +0100

    Small import fix

---
+++ udemy_app/lib/widgets_examples_page.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:udemy_app/widgets_examples/widgets/app_picture.dart';
+import 'package:udemy_app/widgets_examples/widgets/container_text_example.dart';
+import 'package:udemy_app/widgets_examples/widgets/row_expanded_example.dart';
+
+class WidgetsExamplesPage extends StatelessWidget {
+  const WidgetsExamplesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        scrolledUnderElevation: 0.0, //! AppBar doesnt change color after scroll
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        title: const AutoSizeText(
+          "Beschuss Tracking",
+          style: TextStyle(
+            fontFamily: 'BarlowCondensed',
+            fontWeight: FontWeight.w700,
+          ),
+          minFontSize: 30.0,
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+              horizontal: 10, vertical: 20), //! For every Widget in Body
+          child: Column(
+            children: [
+              //* First Widget
+              const AppPicture(),
+
+              //* Second(1) Widget
+              const ContainerTextExample(),
+
+              //* Third(2) Widget
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                child: Material(
+                  elevation: 10,
+                  borderRadius: BorderRadius.circular(20),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: const Color.fromRGBO(239, 174, 63, 0.817),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    height: 100,
+                    child: const Center(
+                      child: AutoSizeText(
+                        "2",
+                        minFontSize: 15.0,
+                        style: TextStyle(
+                          fontFamily: 'BarlowCondensed',
+                          fontSize: 30,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+
+              //* Fourth(3) and Fifth(4) Widget
+              const RowExpandedExample(),
+
+              const ContainerTextExample(),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: Container(
+          height: 60,
+          color: Colors.white,
+          child: Align(
+            child: InkWell(
+              // ignore: avoid_print,
+              onTap: () => print('tap on close'),
+              child: const Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(
+                    Icons.home,
+                    color: Colors.black,
+                  ),
+                  Text(
+                    'Home',
+                    style: TextStyle(
+                        fontFamily: 'BarlowCondensed',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18),
+                  ),
+                ],
+              ),
+            ),
+          )),
+    );
+  }
+}
