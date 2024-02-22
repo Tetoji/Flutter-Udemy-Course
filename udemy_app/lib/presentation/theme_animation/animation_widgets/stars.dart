9467d909c7f91d263e39085814de3365f61260ac
Tetoji <tetjeschulz@web.de>
2024-02-22 21:31:39 +0100

    stars darkmode

--- udemy_app/lib/presentation/theme_animation/animation_widgets/stars.dart
+++ udemy_app/lib/presentation/theme_animation/animation_widgets/stars.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:udemy_app/application/theme_service.dart';
 
 class Stars extends StatelessWidget {
   const Stars({super.key});
@@ -50,6 +52,117 @@ class StarsBig extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+}
+
+class StarPicture extends StatelessWidget {
+  const StarPicture({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<ThemeService>(
+      builder: (context, themeService, child) {
+        return Stack(
+          children: [
+            Positioned(
+              right: 60,
+              child: AnimatedOpacity(
+                  opacity: themeService.isDarkModeOn ? 1 : 0,
+                  duration: const Duration(microseconds: 300),
+                  child: const StarsBig()),
+            ),
+            Positioned(
+              top: 15,
+              right: 67,
+              child: AnimatedOpacity(
+                  opacity: themeService.isDarkModeOn ? 1 : 0,
+                  duration: const Duration(microseconds: 300),
+                  child: const StarsBig()),
+            ),
+            Positioned(
+              top: 30,
+              right: 200,
+              child: AnimatedOpacity(
+                  opacity: themeService.isDarkModeOn ? 1 : 0,
+                  duration: const Duration(microseconds: 300),
+                  child: const StarsBig()),
+            ),
+            Positioned(
+              top: 28,
+              right: 240,
+              child: AnimatedOpacity(
+                  opacity: themeService.isDarkModeOn ? 1 : 0,
+                  duration: const Duration(microseconds: 300),
+                  child: const Stars()),
+            ),
+            Positioned(
+              top: 29,
+              right: 280,
+              child: AnimatedOpacity(
+                  opacity: themeService.isDarkModeOn ? 1 : 0,
+                  duration: const Duration(microseconds: 300),
+                  child: const StarsBig()),
+            ),
+            Positioned(
+              top: 25,
+              right: 300,
+              child: AnimatedOpacity(
+                  opacity: themeService.isDarkModeOn ? 1 : 0,
+                  duration: const Duration(microseconds: 300),
+                  child: const StarsBig()),
+            ),
+            Positioned(
+              top: 150,
+              right: 150,
+              child: AnimatedOpacity(
+                  opacity: themeService.isDarkModeOn ? 1 : 0,
+                  duration: const Duration(microseconds: 300),
+                  child: const Stars()),
+            ),
+            Positioned(
+              top: 130,
+              right: 140,
+              child: AnimatedOpacity(
+                  opacity: themeService.isDarkModeOn ? 1 : 0,
+                  duration: const Duration(microseconds: 300),
+                  child: const Stars()),
+            ),
+            Positioned(
+              top: 130,
+              right: 190,
+              child: AnimatedOpacity(
+                  opacity: themeService.isDarkModeOn ? 1 : 0,
+                  duration: const Duration(microseconds: 300),
+                  child: const Stars()),
+            ),
+            Positioned(
+              top: 100,
+              right: 260,
+              child: AnimatedOpacity(
+                  opacity: themeService.isDarkModeOn ? 1 : 0,
+                  duration: const Duration(microseconds: 300),
+                  child: const StarsBig()),
+            ),
+            Positioned(
+              top: 80,
+              right: 190,
+              child: AnimatedOpacity(
+                  opacity: themeService.isDarkModeOn ? 1 : 0,
+                  duration: const Duration(microseconds: 300),
+                  child: const Stars()),
+            ),
+            Positioned(
+              top: 100,
+              right: 50,
+              child: AnimatedOpacity(
+                  opacity: themeService.isDarkModeOn ? 1 : 0,
+                  duration: const Duration(microseconds: 300),
+                  child: const StarsBig()),
+            ),
+          ],
+        );
+      },
     );
   }
 }
